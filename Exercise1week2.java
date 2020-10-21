fff5146fc60c146a79d3cd80c3797610a709d744
AliaAcha <72845206+AliaAcha@users.noreply.github.com>
2020-10-21 13:21:23 +0800

    Add files via upload

---
+++ Exercise1week2.java
@@ -0,0 +1,49 @@
+/*import java.util.Scanner;*/
+
+public class Exercise1week2 {
+
+	public static void main(String [] arg)
+	{
+
+		/*byte value1 = 127;
+
+		short value2 = 128;
+
+		System.out.println((byte)value2);
+
+		byte value3 = 127;
+
+		value3 += 1;
+		System.out.println(value3);
+
+		value3 += 1;
+		System.out.println(value3);
+
+		String nombor = "100";
+		int intVar1 =nombor ; 
+
+		String nombor = "100";
+		int intVar1 = Integer.parseInt(nombor);
+
+		String nombor + "100";
+		int intVar1 = (int)nombor;*/
+
+		/*Scanner sc = new Scanner(System.in);
+		System.out.println("Enter integer number : ");
+		int x = sc.nextInt();
+		System.out.println("Enter double number : ");
+		double y = sc.nextDouble();
+		System.out.println("Enter string : ");
+		String z = sc.next();
+
+		System.out.println("integer number is : " + x);
+		System.out.println("double number is : " + y);
+		System.out.println("string is : " + z);
+		sc.close();*/
+
+		String numString = "" + 8 + 8 ;
+		System.out.println(numString);
+		
+
+	}
+}
